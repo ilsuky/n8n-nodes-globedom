d9fc0be324462ba3cc10fae707e2f05cddaca640
Christian <suky@tiroler-ac.at>
2023-02-23 12:13:52 +0100

    add axois

--- nodes/GenericFunctions.ts
+++ nodes/GenericFunctions.ts
@@ -5,10 +5,13 @@ import {
 	OptionsWithUri,
 } from 'request';
 
+import axios from 'axios';
+
 import {
 	IDataObject,
 	ILoadOptionsFunctions
 } from 'n8n-workflow';
+
 
 export async function globedomRequest (
 	this: IExecuteFunctions | ILoadOptionsFunctions,
@@ -18,30 +21,8 @@ export async function globedomRequest (
 ) {
 	
 	const credentials = await this.getCredentials('globedom')  as IDataObject;
-	const options: OptionsWithUri = {
-		headers: {
-		},
-		method: 'GET',
-		qs,
-		uri: `${credentials.server}${endpoint}`,
-	};
 
-	const returnr = await this.helpers.request!(options);
-	const dataObject:IDataObject = {};
-	let list = [];
-	const splitData = returnr.split('\n');
-	for(let row of splitData){
-
-	  if(row.includes(":")){
-		const split = row.split(':');
-		dataObject[split[0]] = split[1].trim();
-	  }
-	  else if(row.length >0 ){
-		list.push(row);
-	  }
-	}
-	dataObject.list = list;
-	return dataObject;
+	return credentials;
 }
 
 /**
@@ -50,24 +31,24 @@ export async function globedomRequest (
  export async function getauthtoken (
 	this: IExecuteFunctions | ILoadOptionsFunctions,
 ) {
+	
+	
+	
 	const credentials = await this.getCredentials('globedom') as IDataObject;
 	const body : string = `<request><uid>${credentials.uid}</uid><pwd>${credentials.password}</pwd></request>`;
 
-	const options: OptionsWithUri = {
-		method: 'PUT',
-		headers: {
-			'Content-Type': 'text/xml',
-		},		
-		body,
-		uri: `${credentials.server}:2109/susi/account/login/*/*/*/`,
-		//@ts-ignore
-		resolveWithFullResponse: true,
-		rejectUnauthorized: false,
-	};
+    const { data } = await axios.put(
+      `${credentials.server}:2109/susi/account/login/*/*/*/`,
+      { body },
+      {
+        headers: {
+          'Content-Type': 'text/xml'
+        },
+      },
+    );
 
-	console.log(options);
-	const authsidr = await this.helpers.request!(options);
-	console.log(authsidr);
+    console.log(JSON.stringify(data, null, 4));
+
 	let authsid : string = "";	
 	//console.log(authsid);		
 	return authsid;
